2552d3b5737600845b9962b5172d8db6310d67d5
madhuSudan27 <122591446+madhuSudan27@users.noreply.github.com>
2025-08-29 12:35:09 +0530

    ManyToMany relationship

---
+++ src/main/java/com/spcial/media/models/SocialGroup.java
@@ -0,0 +1,20 @@
+package com.spcial.media.models;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class SocialGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToMany(mappedBy = "groups")
+    private Set<SocialUser> users = new HashSet<>();
+
+
+}
--- src/main/java/com/spcial/media/models/SocialUser.java
+++ src/main/java/com/spcial/media/models/SocialUser.java
@@ -3,7 +3,9 @@ package com.spcial.media.models;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class SocialUser {
@@ -18,5 +20,14 @@ public class SocialUser {
 
     @OneToMany(mappedBy = "user")
     private List<Post> posts = new ArrayList<>();
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "social_user_social_group",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "group_id")
+    )
+    private Set<SocialGroup> groups = new HashSet<>();
 
 }
